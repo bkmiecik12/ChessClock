522a9424a18d2165eee30f4ff6017d37c4e434e7
bkmiecik12 <bkmiecik12@gmail.com>
2017-02-06 17:37:27 +0100

    Class Player added

--- app/src/main/java/com/example/bkmiecik/chessclock/MainActivity.java
+++ app/src/main/java/com/example/bkmiecik/chessclock/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.bkmiecik.chessclock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Player player1;
+    private Player player2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        player1.chronometer = (Button) findViewById(R.id.b_first_player);
+        player2.chronometer = (Button) findViewById(R.id.b_sec_player);
     }
 
 }
---
+++ app/src/main/java/com/example/bkmiecik/chessclock/Player.java
@@ -0,0 +1,16 @@
+package com.example.bkmiecik.chessclock;
+
+import android.widget.Button;
+
+/**
+ * Created by bkmiecik on 06.02.17.
+ */
+public class Player {
+
+    Button chronometer;
+    String name;
+
+    Player(String name){
+        this.name=name;
+    }
+}
