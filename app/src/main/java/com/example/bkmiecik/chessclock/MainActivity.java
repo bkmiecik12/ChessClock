3a3427b8b435e6a56482f5c619c2ca5d1017a103
bkmiecik12 <bkmiecik12@gmail.com>
2017-02-06 17:43:31 +0100

    Class Player changed

--- app/src/main/java/com/example/bkmiecik/chessclock/MainActivity.java
+++ app/src/main/java/com/example/bkmiecik/chessclock/MainActivity.java
@@ -3,19 +3,22 @@ package com.example.bkmiecik.chessclock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
 
     private Player player1;
     private Player player2;
+    private long defaultTime=180000;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        player1.chronometer = (Button) findViewById(R.id.b_first_player);
-        player2.chronometer = (Button) findViewById(R.id.b_sec_player);
+        player1.button = (Button) findViewById(R.id.b_first_player);
+        player2.button = (Button) findViewById(R.id.b_sec_player);
+
     }
 
 }
