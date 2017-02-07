ed4276f6aee75fb50752e8b3a9051eedfa3e9c92
bkmiecik12 <bkmiecik12@gmail.com>
2017-02-07 20:53:35 +0100

    Timer added but doesn't work

--- app/src/main/java/com/example/bkmiecik/chessclock/MainActivity.java
+++ app/src/main/java/com/example/bkmiecik/chessclock/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.bkmiecik.chessclock;
 
+import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Player player1;
     private Player player2;
+    private Button b_switch;
+    private Button b_start;
+    private boolean isStart;
     private long defaultTime=180000;
 
 
@@ -16,8 +24,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        player1.button = (Button) findViewById(R.id.b_first_player);
-        player2.button = (Button) findViewById(R.id.b_sec_player);
+        b_start = (Button) findViewById(R.id.b_start);
+        b_switch = (Button) findViewById(R.id.b_switch);
+        b_start.setText("Start");
+        isStart=false;
+        player1 = new Player(defaultTime,1);
+        player2 = new Player(defaultTime,1);
+        player1.textView = (TextView) findViewById(R.id.c_first_player);
+        player2.textView = (TextView) findViewById(R.id.c_sec_player);
+    }
+    void pressStart(View view){
+        if(!isStart){
+            isStart=true;
+            b_start.setText("Stop");
+            player1.start();
+        }
+        else {
+            player1.stop();
+            player2.cancel();
+            isStart=false;
+            b_start.setText("Start");
+        }
+    }
+    void pressSwitch(View view){
+
 
     }
 
--- app/src/main/java/com/example/bkmiecik/chessclock/Player.java
+++ app/src/main/java/com/example/bkmiecik/chessclock/Player.java
@@ -1,16 +1,56 @@
 package com.example.bkmiecik.chessclock;
 
-import android.widget.Button;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 /**
  * Created by bkmiecik on 06.02.17.
  */
-public class Player {
+public class Player extends CountDownTimer {
 
-    Button chronometer;
-    String name;
+    TextView textView;
+    long millisTillend;
 
-    Player(String name){
-        this.name=name;
+    /**
+     * @param millisInFuture    The number of millis in the future from the call
+     *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+     *                          is called.
+     * @param countDownInterval The interval along the way to receive
+     *                          {@link #onTick(long)} callbacks.
+     */
+    public Player(long millisInFuture, long countDownInterval) {
+        super(millisInFuture, countDownInterval);
+        millisTillend=millisInFuture;
+    }
+
+
+    @Override
+    public void onTick(long millisUntilFinished) {
+        String min = String.format("%02d", millisUntilFinished/60000);
+        int sec = (int)( (millisUntilFinished%60000)/1000);
+        int ms = (int)( (millisUntilFinished%1000));
+        textView.setText(min+":"+String.format("%02d",sec)+"."+String.format("%03d",ms));
+        millisTillend=millisUntilFinished;
+    }
+
+    @Override
+    public void onFinish() {
+            textView.setText("done!");
+    }
+
+    public void setTime(){
+        new Player(millisTillend,1);
+    }
+
+    public void setTextView(){
+        String min = String.format("%02d", millisTillend/60000);
+        int sec = (int)( (millisTillend%60000)/1000);
+        int ms = (int)( (millisTillend%1000));
+        textView.setText(min+":"+String.format("%02d",sec)+"."+String.format("%03d",ms));
+    };
+
+    public void stop(){
+        this.setTime();
+        setTextView();
     }
 }
